8ccbfdc378698224f9346d94890a2fc756580983
Linda Paz <lindacsndelapaz@gmail.com>
2023-06-12 13:56:02 -0300

    add funcionalidad del contador

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -7,19 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title: string= 'Mi primera página';
-  public counter: number = 10;
 
-  increaseBy( value: number ):void {
-    this.counter += value;
-  }
-
-  // opcional
-  decreaseBy(value: number ):void {
-    this.counter -= value;
-  }
-
-  // opcional
-  reset() {
-    this.counter = 10;
-  }
 }
