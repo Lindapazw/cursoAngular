02623ad30f2bdfaa16d1a9838c32f5dcd12784bc
Linda Paz <lindacsndelapaz@gmail.com>
2023-06-14 14:49:56 -0300

    add new proyect con --routing

--- ruteo/src/app/app.module.ts
+++ ruteo/src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-
 
 @NgModule({
   declarations: [
@@ -11,8 +10,7 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    SharedModule,
-
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
