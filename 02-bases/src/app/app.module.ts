815ed4e10c066796e3a82d2c76f11934f35b67a8
Linda Paz <lindacsndelapaz@gmail.com>
2023-06-13 22:15:45 -0300

    fixed modules

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -2,20 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './list/list.component';
 import { DbzModule } from './dbz/dbz.module';
+import { HeroesModule } from './heroes/heroes.module';
+import { CounterModule } from './counter/components/counter/counter.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroComponent,
-    ListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    DbzModule
+    DbzModule,
+    HeroesModule,
+    CounterModule
 
   ],
   providers: [],
