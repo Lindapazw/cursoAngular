d0148abf2225a81121ba3ae216a3656538f3b69a
Linda Paz <lindacsndelapaz@gmail.com>
2023-06-15 12:51:24 -0300

    add finish ruteo

--- ruteo/src/app/auth/auth.module.ts
+++ ruteo/src/app/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { ListComponent } from './pages/list/list.component';
-import { NewPageComponent } from './pages/hero-page/new-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 
 
@@ -13,7 +13,7 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     LoginPageComponent,
     RegisterPageComponent,
     ListComponent,
-    NewPageComponent,
+    HeroPageComponent,
     SearchPageComponent
   ],
   imports: [
