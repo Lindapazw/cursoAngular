f83a555b78ae01c3dd54773a4a12ec09623f925d
Linda Paz <lindacsndelapaz@gmail.com>
2023-06-18 16:50:20 -0300

    delete modal

--- 06-app/src/app/main/pages/inicio-page/inicio-page.component.ts
+++ 06-app/src/app/main/pages/inicio-page/inicio-page.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-inicio-page',
   templateUrl: './inicio-page.component.html',
   styleUrls: ['./inicio-page.component.css']
 })
 export class InicioPageComponent {
-
 }
