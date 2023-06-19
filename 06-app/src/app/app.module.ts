a3d89e089e00c657ae1a7d63a86d6fdb42ab700e
Linda Paz <lindacsndelapaz@gmail.com>
2023-06-19 13:44:50 -0300

    add firebase authentication

--- 06-app/src/app/app.module.ts
+++ 06-app/src/app/app.module.ts
@@ -1,9 +1,12 @@
+// por defecto
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+// ruteo
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+// angular material
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -11,6 +14,9 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -28,6 +34,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     MatIconModule,
     MatListModule,
     NgbModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 06-app/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'itr-bootcamp-auth',
+    appId: '1:337513654001:web:3e78bfaa7f782dc1732b50',
+    storageBucket: 'itr-bootcamp-auth.appspot.com',
+    apiKey: 'AIzaSyA2VwLsjQsViz4POALyqaKrQYfjWTyMIko',
+    authDomain: 'itr-bootcamp-auth.firebaseapp.com',
+    messagingSenderId: '337513654001',
+    measurementId: 'G-W2WTRK4JPM',
+  },
+};
