e83118cd818611c09869d092292d9ceea8d90e1c
Linda Paz <lindacsndelapaz@gmail.com>
2023-06-19 15:51:13 -0300

    add login just ts

--- 06-app/src/app/auth/pages/login/login.component.ts
+++ 06-app/src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor(private router: Router) {}
+
+  login() {
+    const { email, password } = this;
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        console.log('Inicio de sesión exitoso');
+        this.router.navigate(['/main/inicio']); // Redirigir al usuario a "/main/inicio"
+      })
+      .catch((error: any) => {
+        console.error('Error de inicio de sesión:', error);
+      });
+  }
 }
