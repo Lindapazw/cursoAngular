1e8f9a4e460035bcc6d476dd9ce81b9e8dd82a7f
Linda Paz <lindacsndelapaz@gmail.com>
2023-06-20 10:42:34 -0300

    add app.component employeArray

--- crud/src/app/app.component.ts
+++ crud/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { Employee } from './models/employee';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crud';
+  employeeArray: Employee[] = [
+    { id: 1, name: 'John', country: 'United States'},
+    { id: 2, name: 'Linda', country: 'United States'},
+    { id: 3, name: 'Angel', country: 'United States'},
+  ];
 }
