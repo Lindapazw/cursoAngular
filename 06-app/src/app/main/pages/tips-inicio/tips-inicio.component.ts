7453060656e33ae8a9d25bcc9065ada832fe468c
Linda Paz <lindacsndelapaz@gmail.com>
2023-06-21 19:47:05 -0300

    add tips component

---
+++ 06-app/src/app/main/pages/tips-inicio/tips-inicio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tips-inicio',
+  templateUrl: './tips-inicio.component.html',
+  styleUrls: ['./tips-inicio.component.css']
+})
+export class TipsInicioComponent {
+
+}
