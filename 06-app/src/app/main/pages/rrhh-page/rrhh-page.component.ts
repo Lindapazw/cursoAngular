79ef587ca674df11733269980be6ca4435ac6e46
Linda Paz <lindacsndelapaz@gmail.com>
2023-06-21 19:40:09 -0300

    add rrhh table

--- 06-app/src/app/main/pages/rrhh-page/rrhh-page.component.ts
+++ 06-app/src/app/main/pages/rrhh-page/rrhh-page.component.ts
@@ -51,25 +51,3 @@ export class RrhhPageComponent implements OnInit {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
-
-// export interface Employee {
-//   position: number;
-//   nombre: string;
-//   apellido: string;
-//   dni: number;
-//   role: string;
-//   senority: string;
-//   sex: string;
-// }
-
-// {position: 1, nombre: 'Linda',  apellido: 'Paz', dni: 42379856, role: 'Angular developer jr.',senority:'junior', sex: 'F'},
-// {position: 2, nombre: 'Juan', apellido: 'Martínez', dni: 37462854, role: 'diseñador gráfico',senority:'ceo',sex: 'M'},
-// {position: 3, nombre: 'Ana', apellido: 'González', dni: 28675321, role: 'analista financiero',senority:'semi senior',sex: 'F'},
-// {position: 4, nombre: 'Carlos', apellido: 'López', dni: 40986573, role: 'gerente de ventas',senority:'senior', sex: 'F'},
-// {position: 5, nombre: 'María', apellido: 'Rodríguez', dni: 56847239, role: 'especialista en marketing',senority:'ceo', sex: 'F'},
-// {position: 6, nombre: 'Pedro', apellido: 'Sánchez', dni: 62548917, role: 'consultor de negocios',senority:'semi senior', sex: 'M'},
-// {position: 7, nombre: 'Laura', apellido: 'Torres', dni: 34987562, role: 'desarrollador web' ,senority:'junior', sex: 'F'},
-// {position: 8, nombre: 'Manuel', apellido: 'Hernández', dni: 48762319, role: 'analista de datos',senority:'ceo', sex: 'F'},
-// {position: 9, nombre: 'Isabel', apellido: 'Díaz', dni: 72983415, role: 'project manager',senority:'junior', sex: 'F'},
-// {position: 10, nombre: 'Fernando', apellido: 'Gómez', dni: 51627398, role: 'arquitecto de software',senority:'ceo', sex: 'M'},
-// {position: 11, nombre: 'Sofía', apellido: 'Vargas', dni: 64178235, role: 'especialista en recursos humanos',senority:'semi senior', sex: 'F'}
