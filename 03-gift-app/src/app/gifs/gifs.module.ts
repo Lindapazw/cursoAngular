d1ca9edba5bc118b3f8d208393476a3c7cbc7db9
Linda Paz <lindacsndelapaz@gmail.com>
2023-06-24 11:18:10 -0300

    add search-box

--- 03-gift-app/src/app/gifs/gifs.module.ts
+++ 03-gift-app/src/app/gifs/gifs.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    SearchBoxComponent
   ],
   imports: [
     CommonModule
