e890915497bf0505d07ebd59195f9eb29e298b43
Linda Paz <lindacsndelapaz@gmail.com>
2023-06-24 11:31:45 -0300

    add card component

--- 03-gift-app/src/app/gifs/gifs.module.ts
+++ 03-gift-app/src/app/gifs/gifs.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardComponent } from './components/card/card.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    SearchBoxComponent
+    SearchBoxComponent,
+    CardComponent
   ],
   imports: [
     CommonModule
